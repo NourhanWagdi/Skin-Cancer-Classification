8c3c632d8736cb8c45c29e78224d3db7c855d49b
hunny9891 <himanshu_thakur9891@live.in>
2020-04-05 16:39:29 +0530

    changes

--- notebook.py
+++ notebook.py
@@ -11,108 +11,26 @@ from tensorflow.keras.preprocessing import image
 
 
 # %%
+lesion_type_dict = {
+    'nv': 'Melanocytic nevi',
+    'mel': 'dermatofibroma',
+    'bkl': 'Benign keratosis-like lesions ',
+    'bcc': 'Basal cell carcinoma',
+    'akiec': 'Actinic keratoses',
+    'vasc': 'Vascular lesions',
+    'df': 'Dermatofibroma'
+}
+
+
+# %%
 df = pd.read_csv(util.METADATA_PATH)
 print(df.head())
 
 
 # %%
-df.isnull().sum()
-
-
-# %%
-df['age'].fillna(df['age'].mean(), inplace=True)
-
-
-# %%
-stats = df['dx'].value_counts()
-
-
-# %%
-# import seaborn as sns
-
-
-# %%
-plt.figure(figsize=(10,6))
-plt.title('Count of data based on catoegory.')
-# sns.barplot(x=stats.index, y=stats)
-plt.ylabel("Number of samples.")
-
-
-# %%
-dx_type_stats = df['dx_type'].value_counts()
-
-
-# %%
-# sns.barplot(x=dx_type_stats.index, y=dx_type_stats)
-
-
-# %%
-localization_stats = df['localization'].value_counts()
-
-
-# %%
-# sns.barplot(x=localization_stats.index, y=localization_stats)
-
-
-# %%
-sex_stats = df['sex'].value_counts()
-# sns.barplot(x=sex_stats.index, y=sex_stats)
-
-
-# %%
+df['cell_type'] = df['dx'].map(lesion_type_dict.get)
+df['cell_type_idx'] = pd.Categorical(df['cell_type']).codes
 df.head()
-
-
-# %%
-df['dx_type'].unique()
-
-
-# %%
-df['localization'].unique()
-
-
-# %%
-dx_type_dict = {
-    'histo': 0,
-    'consensus': 1,
-    'confocal': 2,
-    'follow_up': 3
-}
-
-localization_dict = {
-    'scalp': 0,
-    'ear': 1,
-    'face': 1,
-    'back': 3,
-    'trunk': 4,
-    'chest': 5,
-    'upper extremity': 6,
-    'abdomen': 7,
-    'lower extremity': 6,
-    'genital': 2,
-    'neck': 8,
-    'hand': 9,
-    'foot': 10,
-    'acral': 11,
-    'unknown': 12
-}
-
-df['localization'] = df['localization'].map(localization_dict)
-df['dx_type'] = df['dx_type'].map(dx_type_dict)
-df.head()
-
-
-# %%
-sex_map = {
-    'male': 0,
-    'female': 1
-}
-df['sex'] = df['sex'].map(sex_map)
-df.head()
-
-
-# %%
-samimg = image.load_img(os.path.join(util.IMAGE_PATH, df['image_id'][0] + '.jpg'))
 
 
 # %%
@@ -147,84 +65,94 @@ df_undup.shape
 
 
 # %%
-
-
-
-# %%
-samimg.resize((100,75))
-
-
-# %%
-df_undup['image'] = df_undup['image_id'].map(
-    lambda x: np.asarray(image.load_img(
-        os.path.join(util.IMAGE_PATH, x + ".jpg")
-    ).resize((75,75)))
-)
-
-
-# %%
-df = df_undup
-df_undup.shape
-
-
-# %%
-df['image'] = df['image']/255
-df.head()
-
-
-# %%
-df['dx'].value_counts()
-
-
-# %%
-X = df[['dx_type', 'age', 'sex', 'localization','image']]
-
-
-# %%
-X.head()
-
-
-# %%
-Y = df['dx']
-
-
-# %%
-Y.head()
-
-
-# %%
-Y.unique()
-
-
-# %%
-ymap = {
-    'bkl':0, 
-    'nv':1, 
-    'df':2, 
-    'mel':3, 
-    'vasc':4, 
-    'bcc':5, 
-    'akiec':6
-}
-
-Y = Y.map(ymap)
-Y.head()
-
-
-# %%
 # Train Test Split Data
 from sklearn.model_selection import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y,test_size=0.2, random_state=42)
+_, df_test = train_test_split(df_undup,test_size=0.2, random_state=42)
+df_test.shape
 
 
 # %%
-X_train.count()
+def get_val_rows(x):
+    test_list = list(df_test['image_id'])
+    if str(x) in test_list:
+        return 'test'
+    else:
+        return 'train'
+
+df['train_or_test'] = df['image_id']
+df['train_or_test'] = df['train_or_test'].apply(get_val_rows)
+df_train = df[df['train_or_test'] == 'train']
+df_test =df[df['train_or_test'] == 'test']
+
+
+# %%
+df_train.shape
+
+
+# %%
+df_test.shape
+
+
+# %%
+# Split test data into validation data
+df_test, df_val = train_test_split(df_test, test_size=0.3, random_state=101)
+df_val.shape
+
+
+# %%
+df_test.shape
+
+
+# %%
+df_train['dx'].value_counts()
+
+# %%
+# Function to load images.
+def load_img(df):
+    df['image'] = df['image_id'].map(
+        lambda x: np.asarray(image.load_img(
+            os.path.join(util.IMAGE_PATH, x + ".jpg")
+        ).resize((224,224)))
+    )
+    return df
+
+
+# %%
+df_train = load_img(df_train)
+df_test = load_img(df_test)
+df_val = load_img(df_val)
+
+
+# %%
+# Copy fewer class to balance the number of 7 classes
+data_aug_rate = [15,10,5,50,0,40,5]
+for i in range(7):
+    if data_aug_rate[i]:
+        df_train=df_train.append([df_train.loc[df_train['cell_type_idx'] == i,:]]*(data_aug_rate[i]-1),                                                 ignore_index=True)
+
+
+# %%
+df_train['dx'].value_counts()
+
+
+# %%
+Y_train = df_train['dx']
+
+
+# %%
+Y_test = df_test['dx']
+Y_val = df_val['dx']
+
+
+# %%
+Y_val.head()
 
 
 # %%
 # Normalize data
-X_train = np.asarray(X_train['image'].to_list())
-X_test = np.asarray(X_test['image'].to_list())
+X_train = np.asarray(df_train['image'].to_list())
+X_test = np.asarray(df_test['image'].to_list())
+X_val = np.asarray(df_val['image'].to_list())
 
 
 # %%
@@ -245,6 +173,8 @@ X_train = (X_train - train_mean) / train_std
 # %%
 from tensorflow.keras.utils import to_categorical
 Y_train = to_categorical(Y_train, num_classes=7)
+Y_test = to_categorical(Y_test, num_classes=7)
+Y_val = to_categorical(Y_val, num_classes=7)
 
 
 # %%
@@ -252,42 +182,17 @@ Y_train.shape
 
 
 # %%
-# Separate validation test
-X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size=0.1, random_state=42)
-
-
-# %%
 from custom_model import naiveModel, get_inception_model
-model = get_inception_model(input_shape=(75,75,3))
+model = get_inception_model(input_shape=(224,224,3))
 
 
 # %%
 from train import train
-history = train(model, X_train, Y_train, X_val, Y_val, 50, 8, False)
-
-
-# %%
-type(history.history)
-
-
-# %%
-Y_test = to_categorical(Y_test, num_classes=7)
-
+history = train(model, X_train, Y_train, X_val, Y_val, 2, 8, False)
 
 # %%
 _, test_acc = model.evaluate(X_test, Y_test)
 print("Accuracy on the test set = ", str(test_acc * 100))
-
-
-# %%
-history_df = pd.DataFrame([history.history['acc'], history.history['val_acc']])
-
-
-# %%
-# sns.lineplot(data=history_df)
-
-
-# %%
 
 
 
