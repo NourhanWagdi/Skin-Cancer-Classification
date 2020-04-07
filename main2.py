be7e5d23b6d48e5c224a3e29fc6713c96e18dbe7
hunny9891 <himanshu_thakur9891@live.in>
2020-04-07 23:34:59 +0530

    changes

--- main2.py
+++ main2.py
@@ -13,4 +13,4 @@ VAL_DIR = os.path.join(
     util.SKIN_CANCER_HAMNIST_HAM_1000_PATH, 'arrangedData/validation')
 
 model = custom_model.get_inception_model(input_shape=(224,224,3))
-history = train.trainRaw(model, TRAINING_DIR, VAL_DIR, TESTING_DIR, epochs=5)
+history = train.trainRaw(model, TRAINING_DIR, TESTING_DIR, VAL_DIR, epochs=50)
